54192b550c27d9122ec1cd133214030b453c1e58
TIIGRUS <>
2020-06-09 16:26:28 +0300

    End desktop

---
+++ js/main.js
@@ -0,0 +1,13 @@
+let accToggle = document.getElementsByClassName('accordion__title');
+
+for (let i = 0; i < accToggle.length; i++) {
+    accToggle[i].addEventListener('click', function () {
+        this.classList.toggle('accordion__title_active');
+        let accContent = this.nextElementSibling;
+        if (accContent.style.maxHeight) {
+            accContent.style.maxHeight = null;
+        } else {
+            accContent.style.maxHeight = accContent.scrollHeight + "px";
+        }
+    });
+}
